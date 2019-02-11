31a23e061d3210439e04feb32dd7d6fe6f08cb4f
Btagayaliev <32046645+Btagayaliev@users.noreply.github.com>
2019-02-11 01:29:13 +0100

    Update Shop.java

--- OnlineShop/Shop.java
+++ OnlineShop/Shop.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 
 /**
  *
@@ -12,6 +8,7 @@ import java.util.Scanner;
 
 public class Shop {
 
+    //private fields
     private Storehouse store;
     private Scanner reader;
 
